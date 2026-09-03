9b7d05194543aed3dbcc594bf89bfd764151eedc
Kutikov Pasha <kutikovpasha@gmail.com>
2026-09-03 09:39:06 +0600

    123

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,24 @@
 const App = () => {
-  const username = "ktkv419"
+    const username = 'ktkv419'
 
-  return (
-    <h1>С подключением, {username}</h1>
-  )
+    const handleMinus = () => {
+
+    }
+
+    const handlePlus = () => {
+
+    }
+
+    return (
+        <>
+            <h1>С подключением, {username}</h1>
+            <div>
+                <button onClick={handleMinus}>-</button>
+                <span>0</span>
+                <button onClick={handlePlus}>+</button>
+            </div>
+        </>
+    )
 }
 
 export default App
